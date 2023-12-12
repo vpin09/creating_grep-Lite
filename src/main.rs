75ae044cd57972d76f5f5df6e59e63e75cc60573
vpin09 <sainivipin1998@gmail.com>
2023-12-12 20:58:40 +0530

    regex crate

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
+use regex::Regex;
 fn main() {
-   let search_term="picture";
+   let re=Regex::new("picture").unwrap();
    let quote=" \
    Every face, every shop, bedroom window, public-house, and
    dark square is a picture feverishly turned--in search of what?
    It is the same with books. What do we seek through millions of pages?";
    // let mut line_num=1;
-   for (i,line)  in quote.lines().enumerate() {
-      if line.contains(search_term) {
-         let mut line_num=i+1;
-
-         println!("{}, {}",line_num,line);
+   for line in quote.lines() {
+      let contain_substring=re.find(line);
+      match contain_substring {
+         Some(_) => println!("{}",line),
+         None =>(),
       }
+      
    }
 
 
