a8f199064abc0550329ab0f66ae28ba836724e1a
vpin09 <sainivipin1998@gmail.com>
2023-12-12 21:21:33 +0530

    clap crate

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
+use std::env::args;
 use regex::Regex;
+use clap::{App,Arg};
+
 fn main() {
-   let re=Regex::new("picture").unwrap();
-   let quote=" \
-   Every face, every shop, bedroom window, public-house, and
+   let args=App::new("grep-lite").version("0.1").about("Search for patterns").arg(
+      Arg::with_name("pattern").help("The pattern to search for").takes_value(true).required(true)
+   ).get_matches();
+   let pattern=args.value_of("pattern").unwrap();
+
+   let re=Regex::new(pattern).unwrap();
+   let quote="Every face, every shop, bedroom window, public-house, and
    dark square is a picture feverishly turned--in search of what?
    It is the same with books. What do we seek through millions of pages?";
    // let mut line_num=1;
    for line in quote.lines() {
-      let contain_substring=re.find(line);
-      match contain_substring {
+     ;
+      match re.find(line) {
          Some(_) => println!("{}",line),
          None =>(),
       }
